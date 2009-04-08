7712b4f4f1c784dc023357cc71299ac9c4cba882
Ehren Murdick <ehren@theedgecase.com>
2009-04-09 02:56:49 +0800

    Incorporate Peejs change to make it play nice in noConflict mode
    Signed-off-by: Adam McCrea <adam@theedgecase.com>

--- auto_update_stylesheets.js
+++ auto_update_stylesheets.js
@@ -1,60 +1,62 @@
-$.autoUpdateStylesheets = function(enable_or_disable) {
-  var linkElements = $('head link'), index = -1;
+(function($) {
+  $.autoUpdateStylesheets = function(enable_or_disable) {
+    var linkElements = $('head link'), index = -1;
 
-  if (enable_or_disable == 'disable') {
-    stop();
-    return
-  }
+    if (enable_or_disable == 'disable') {
+      stop();
+      return
+    }
 
-  if (!data('initialized')) {
-    normalizeQueryStrings();
-    data("initialized", true);
-  }
-  start();
+    if (!data('initialized')) {
+      normalizeQueryStrings();
+      data("initialized", true);
+    }
+    start();
 
-  function compareNextStyleSheet() {
-    if (data('timeout') == false) return;
-    var link = getNextLink(),
-        url = link.href.replace(/timestamp=\d+/, 'timestamp=' + new Date().getTime());
+    function compareNextStyleSheet() {
+      if (data('timeout') == false) return;
+      var link = getNextLink(),
+          url = link.href.replace(/timestamp=\d+/, 'timestamp=' + new Date().getTime());
 
-    $.ajax({
-      url: url,
-      success: function(data) { 
-        if (data != $(link).data('cachedData')) {
-          $(link).data('cachedData', data).attr('href', url);
+      $.ajax({
+        url: url,
+        success: function(data) { 
+          if (data != $(link).data('cachedData')) {
+            $(link).data('cachedData', data).attr('href', url);
+          }
+        }, 
+        complete: function() {
+          data('timeout', setTimeout(compareNextStyleSheet, 100));
         }
-      }, 
-      complete: function() {
+      });
+    }
+
+    function data(name, val) {
+      return linkElements.data("autoUpdateStylesheets." + name, val);
+    }
+
+    function stop() {
+      clearTimeout(data('timeout'));
+      data('timeout', false);
+    }
+
+    function start() {
+      if (!data("timeout"))
         data('timeout', setTimeout(compareNextStyleSheet, 100));
-      }
-    });
-  }
+    }
 
-  function data(name, val) {
-    return linkElements.data("autoUpdateStylesheets." + name, val);
-  }
+    function getNextLink() {
+      index = (index + 1) % linkElements.length;
+      return linkElements[index];
+    }
 
-  function stop() {
-    clearTimeout(data('timeout'));
-    data('timeout', false);
+    function normalizeQueryStrings() {
+      linkElements.each(function(i, link) {
+        var url = link.href;
+        if (url.indexOf('?') == -1) url += '?';
+        url += '&timestamp=0000';
+        $(link).attr('href', url);
+      });
+    }
   }
-
-  function start() {
-    if (!data("timeout"))
-      data('timeout', setTimeout(compareNextStyleSheet, 100));
-  }
-
-  function getNextLink() {
-    index = (index + 1) % linkElements.length;
-    return linkElements[index];
-  }
-
-  function normalizeQueryStrings() {
-    linkElements.each(function(i, link) {
-      var url = link.href;
-      if (url.indexOf('?') == -1) url += '?';
-      url += '&timestamp=0000';
-      $(link).attr('href', url);
-    });
-  }
-}
+})(jQuery);
